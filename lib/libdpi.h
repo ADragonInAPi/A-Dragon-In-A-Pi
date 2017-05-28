0663742656327d652fbbc8ba37c77243bf0f8f7f
Kyle Amoroso <kamoroso1994@gmail.com>
2017-05-28 18:47:44 -0400

    Add include guard

--- lib/libdpi.h
+++ lib/libdpi.h
@@ -2,9 +2,14 @@
  * libdpi.h - for function definitions from libdpi.c
  */
 
+#ifndef LIBDPI_H
+#define LIBDPI_H
+
 #include <wiringPi.h>
 #include <errno.h>
 #include <wiringSerial.h>
 
-int setPos(int motor, unsigned int position)
+int setPos(int motor, unsigned int position);
 int resetmotors(void);
+
+#endif
