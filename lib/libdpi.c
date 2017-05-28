41406849e021549e3f3761dc0509e745db93c38a
Nkeck720 <noahkeck72@gmail.com>
2017-05-28 11:00:43 -0400

    Accepted changes, see comments
    From now on, any suggested changes need to be made on a separate branch
    with a pull request to maximize efficiency, so that we don't have
    commits that linger on master with possible issues.
    
    Signed-off-by: Nkeck720 <noahkeck72@gmail.com>

--- lib/libdpi.c
+++ lib/libdpi.c
@@ -3,7 +3,11 @@
  * 			  As of current, this code is under the public domain.
  */
 
-/* #include "libdpi.h" */   // Why aren't we using this?
+/* 
+ *  libdpi.h isn't necessary here because the functions are already defined in this file. We can use it, but the 
+ *  compiler may not like it.
+ */
+#include "libdpi.h"
 #include <errno.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
@@ -14,40 +18,36 @@
 
 int setPos(int motor, unsigned int position) {
 	// Set the motor's enable pin high
-    int motorPin = (motor == 15) ? 16 : motor;
+        int motorPin = (motor == 15) ? 16 : motor;
 	int serial = serialOpen("/dev/ttyAMA0", 300);
-
 	if(serial == -1) {
 		return 1;
 	}
-
 	digitalWrite(motorPin, 1);
 	serialPutchar(serial, (unsigned char) position);
-	digitalWrite(notorPin, 0);
+	digitalWrite(motorPin, 0);
 	serialClose(serial);
-
 	return 0;
 }
 
 /*
  * resetmotors() - resets all servos to their default positions. (Note: we can set these as desired when we get a prototype.
- *				   For now, this just sets them all to zero.) Must be called before any other function as it also sets up GPIO.
+ *		   For now, this just sets them all to zero.) Must be called before any other function as it also sets up GPIO.
  */
 
 int resetmotors(void) {
     int i;
-
-	if(wiringPiSetup() == -1) {
-		return 1; 			// Fail on setup not working
-	}
-
-    // would it be safe to combine the two loops?
+    if(wiringPiSetup() == -1) {
+        return 1; 			// Fail on setup not working
+    }
     for(i = 0; i <= 14; i++) {
         pinMode(i, OUTPUT);
     }
-	pinMode(16, OUTPUT);   // was this a bug?
+    // This is not a bug. I did this because we won't be using the UART's Rx pin as of yet, so I simply set it
+    // to be used as a motor enable line to maximize the number of servos we could possibly use.
+    pinMode(16, OUTPUT);   
 
-	// Now set the motors to defaults
+    // Now set the motors to defaults
     for(i = 0; i <= 15; i++) {
         setPos(i, 0);
     }
